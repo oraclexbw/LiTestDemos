c7d1117025d1705d2798e6011d50a3cb7ede9b40
liwujun <lwj@kuba.bz>
2014-09-15 14:12:48 +0800

    first li demos commit

---
+++ src/com/example/litestdemos/DrawerTestActivity.java
@@ -0,0 +1,12 @@
+package com.example.litestdemos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawerTestActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.drawer_layout_test);
+	}
+
+}
